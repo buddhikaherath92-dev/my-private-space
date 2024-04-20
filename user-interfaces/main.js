5da6a478c9b0af20d5da8dcce1fe0a7c06586c00
Buddhika Herath <buddhikaherath.dev@gmail.com>
2024-04-20 22:01:14 +0400

    Small home page for fun

---
+++ user-interfaces/main.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+              'headings': ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+            },
+          }
+    }
+  }
